cfe50b7e001859b9d081f84c2dc6415d58a16874
Michael Lobman <michael.lobman@gmail.com>
2021-10-17 18:40:26 -0400

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,8 @@
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
+document.querySelector("body").appendChild(h2);
+
+const p = document.createElement("p");
+p.textContent = "This content added for posterity";
+document.querySelector("body").appendChild(p);
+
